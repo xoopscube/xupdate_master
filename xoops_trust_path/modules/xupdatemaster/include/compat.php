6e7ba84259f6daa7c6af5e7c9cfe3afdbee09746
nao-pon <hypweb@gmail.com>
2013-05-23 14:01:49 +0900

    fix compat function parse_ini_string()

--- xoops_trust_path/modules/xupdatemaster/include/compat.php
+++ xoops_trust_path/modules/xupdatemaster/include/compat.php
@@ -25,9 +25,9 @@ if(!function_exists('parse_ini_string')){
 		$inSect = false;
 		foreach($lines as $line){
 			$line = trim($line);
-			if(!$line || $line[0] == "#" || $line[0] == ";")
+			if(!$line || $line[0] == '#' || $line[0] == ';')
 				continue;
-			if($line[0] == "[" && $endIdx = strpos($line, "]")){
+			if($line[0] == '[' && $endIdx = strpos($line, ']')){
 				$inSect = substr($line, 1, $endIdx-1);
 				continue;
 			}
@@ -36,16 +36,39 @@ if(!function_exists('parse_ini_string')){
 			 
 			$tmp = explode('=', $line, 2);
 			$key = trim($tmp[0]);
+			$arrayKey = null;
+			
+			if (preg_match('/^([^\[\]]+)\[([^\]]*)\]$/', $key, $_match)) {
+				$key = $_match[1];
+				$arrayKey = $_match[2];
+			}
 			
 			if (preg_match('/[?{}|&~!\[()^"]/', $key)) continue;
 			
 			if ($key) {
 				$val = ltrim($tmp[1]);
 				$val = preg_replace('/^"(.*)"$/', '$1', $val);
-				if($ProcessSections && $inSect)
-					$return[$inSect][$key] = $val;
-				else
-					$return[$key] = $val;
+				if($ProcessSections && $inSect) {
+					if (is_null($arrayKey)) {
+						$return[$inSect][$key] = $val;
+					} else {
+						if ($arrayKey !== '') {
+							$return[$inSect][$key][$arrayKey] = $val;
+						} else {
+							$return[$inSect][$key][] = $val;
+						}
+					}
+				} else {
+					if (is_null($arrayKey)) {
+						$return[$key] = $val;
+					} else {
+						if ($arrayKey !== '') {
+							$return[$key][$arrayKey] = $val;
+						} else {
+							$return[$key][] = $val;
+						}
+					}
+				}
 			}
 		}
 		return $return;
