a3ea18111bd4ab7234694f73db7c2e6030fe0ff5
nao-pon <hypweb@gmail.com>
2013-05-23 11:35:57 +0900

    add compat function parse_ini_string()

--- xoops_trust_path/modules/xupdatemaster/include/compat.php
+++ xoops_trust_path/modules/xupdatemaster/include/compat.php
@@ -17,3 +17,29 @@ if (! extension_loaded('json')) {
 		}
 	}
 }
+
+if(!function_exists('parse_ini_string')){
+	function parse_ini_string($str, $ProcessSections=false){
+		$lines  = explode("\n", $str);
+		$return = Array();
+		$inSect = false;
+		foreach($lines as $line){
+			$line = trim($line);
+			if(!$line || $line[0] == "#" || $line[0] == ";")
+				continue;
+			if($line[0] == "[" && $endIdx = strpos($line, "]")){
+				$inSect = substr($line, 1, $endIdx-1);
+				continue;
+			}
+			if(!strpos($line, '=')) // (We don't use "=== false" because value 0 is not valid as well)
+				continue;
+			 
+			$tmp = explode("=", $line, 2);
+			if($ProcessSections && $inSect)
+				$return[$inSect][trim($tmp[0])] = ltrim($tmp[1]);
+			else
+				$return[trim($tmp[0])] = ltrim($tmp[1]);
+		}
+		return $return;
+	}
+}
